10b855d2093d1e54b9b1dc225539a2b0a151d5a8
Cal Colson <cpcolson@yahoo.com>
2020-12-19 13:34:06 -0600

    update ytdl-core

--- tests/ytdlTest.js
+++ tests/ytdlTest.js
@@ -1,6 +1,7 @@
 const ytdl = require('ytdl-core')
 
-const videoId = 'fHI8X4OXluQ'
+const videoId = 'fHI8X4OXluQ' // Blinding Lights
+// const videoId = 'sBl3QrOqsRY' // King of the Hill
 
 ytdl.getInfo('https://www.youtube.com/watch?v=' + videoId).then((data) => {
   console.log(data.videoDetails.title)
